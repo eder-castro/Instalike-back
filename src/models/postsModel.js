fb3ce2c0990298e745d230facd7545322ab958c3
eder-castro <132703429+eder-castro@users.noreply.github.com>
2024-11-24 14:00:47 -0300

    Subindo projeto da Imersão Dev Back-End da Alura
    Enviando projeto local para o Github

---
+++ src/models/postsModel.js
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import { ObjectId } from "mongodb";
+import conectarAoBanco from "../config/dbConfig.js";
+
+// Conecta ao banco de dados MongoDB usando a string de conexão do ambiente
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
+
+// Função assíncrona para obter todos os posts do banco de dados
+export async function getTodosPosts() {
+    // Seleciona o banco de dados "imersao-instabytes" 
+    const db = conexao.db("imersao-instabytes")
+    // Seleciona a coleção "posts" dentro do banco de dados
+    const colecao = db.collection("posts")
+    // Busca todos os documentos (posts) na coleção e retorna como um array
+    return colecao.find().toArray()
+}
+
+export async function criarPost(novoPost) {
+    // Seleciona o banco de dados "imersao-instabytes" 
+    const db = conexao.db("imersao-instabytes")
+    // Seleciona a coleção "posts" dentro do banco de dados
+    const colecao = db.collection("posts")
+    // Busca todos os documentos (posts) na coleção e retorna como um array
+    return colecao.insertOne(novoPost)
+}
+
+export async function atualizarPost(id, novoPost) {
+    // Seleciona o banco de dados "imersao-instabytes" 
+    const db = conexao.db("imersao-instabytes")
+    // Seleciona a coleção "posts" dentro do banco de dados
+    const colecao = db.collection("posts")
+    // Busca todos os documentos (posts) na coleção e retorna como um array
+    const objId = ObjectId.createFromHexString(id)
+    return colecao.updateOne({_id: new ObjectId(objId)},{$set: novoPost})
+}
---
+++ src/routes/postRoutes.js
@@ -0,0 +1,37 @@
+import cors from "cors";
+import express from "express";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({dest: "./uploads", storage})
+
+//Rota para buscar todos os posts
+const routes = (app) => {
+    // Habilita o parsing de JSON no corpo das requisições
+    app.use(express.json());
+    app.use(cors(corsOptions));
+    // Define uma rota GET para obter todos os posts
+    app.get("/posts", listarPosts);
+    // Define uma rota POST para criar um post
+    app.post("/posts", postarNovoPost);
+    //Define uma rota POST para upload de imagens
+    app.post("/upload", upload.single("imagem"), uploadImagem);
+    //Define uma rota PUT para atualizar um post
+    app.put("/upload/:id", atualizarNovoPost)
+}
+
+export default routes;
